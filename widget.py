0af9e468c2dd58569cdeafe7550f170a8abfc77f
adnksharp <darkmark404@protonmail.com>
2024-12-08 18:18:07 -0700

    feat: form connections defined.

--- widget.py
+++ widget.py
@@ -18,9 +18,37 @@ class Board():
 
 class Widget(QWidget):
     def __init__(self, parent=None):
+        self.status = False
+        
         super().__init__(parent)
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
+        
+        self.ui.pwmSlider.valueChanged.connect(self.updateSpin)
+        self.ui.pwmSpin.valueChanged.connect(self.updateSlider)
+        self.ui.drive.clicked.connect(self.run)
+        self.ui.reverse.clicked.connect(self.reverse)
+        self.ui.stop.clicked.connect(self.stop)
+        
+    def updateSpin(self):
+        self.ui.pwmSpin.setValue(self.ui.pwmSlider.value())
+        
+    def updateSlider(self):
+        self.ui.pwmSlider.setValue(self.ui.pwmSpin.value())
+        
+    def run(self):
+        if not self.status:
+            self.status = True
+            self.ui.lcdNumber.display(round((self.ui.pwmSlider.value() * 0.12), 2))
+            
+    def reverse(self):
+        if not self.status:
+            self.status = True
+            self.ui.lcdNumber.display(round(-(self.ui.pwmSlider.value() * 0.12), 2))
+            
+    def stop(self):
+        self.status = False
+        
 
 
 if __name__ == "__main__":
